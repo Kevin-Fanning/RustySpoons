8cd04fb43f0eeedaec90dbba0ed1841d58de728e
Kevin Fanning <kmf5302@gmail.com>
2013-09-07 12:05:38 -0400

    early morning commit

--- rusty_spoons/src/com/me/mygdxgame/Screen.java
+++
@@ -1,9 +0,0 @@
-package com.me.mygdxgame;
-
-public interface Screen {
-	public void pause();
-	public void resume();
-	public void render(float delta);
-	public void show();
-	public void resize(int width,int height);
-}
